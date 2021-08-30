1d0e097739d6f6734473837563b39f5d1d290dd6
Diederick <diederick.de.buck@gmail.com>
2021-08-30 12:37:16 +0200

    Initial Commit

---
+++ TicTac/TicTacApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacApp.swift
+//  TicTac
+//
+//  Created by Diederick de Buck on 30/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct TicTacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
